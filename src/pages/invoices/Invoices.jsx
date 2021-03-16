e29af09eb74a8d508312e659dec47c6e13c3fbff
elshanx <elshanxakbarov@gmail.com>
2021-03-16 23:17:55 +0400

    fixed naming

--- src/pages/invoices/Invoices.jsx
+++ src/pages/invoices/Invoices.jsx
@@ -2,10 +2,18 @@ import InvoiceDrawer from '../../components/invoiceDrawer/InvoiceDrawer';
 import InvoiceList from '../../components/invoice-list/InvoiceList';
 import { InvoicesContainer } from './Invoices.styles';
 import InvoicesHeader from '../../components/invoices-header/InvoicesHeader';
+import { fetchInvoicesStartAsync } from '../../redux/invoices/invoices.actions';
+import { useDispatch } from 'react-redux';
+import { useEffect } from 'react';
 import { useRouteMatch } from 'react-router-dom';
 
 const Invoices = () => {
   const match = useRouteMatch('/invoices/new');
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(fetchInvoicesStartAsync());
+  }, []);
 
   const body = (
     <InvoicesContainer>
