da9b10914afe6b42b4bdf631578235d7be1b29c1
elshanx <elshanxakbarov@gmail.com>
2021-03-29 18:26:34 +0400

    🚀 feat: adding dynamic formik objects and validating them on the fly

--- src/components/form/FormItemListInputs.jsx
+++ src/components/form/FormItemListInputs.jsx
@@ -1,47 +1,69 @@
-import { Field } from 'formik';
-import { Flex, ItemInputs } from './Form.styles';
+import { Field, FieldArray } from 'formik';
+import icon from '../../assets/trash-icon.svg';
+import { Button } from '../common';
+import { Flex, Icon, IconContainer, ItemInputs, Sum } from './Form.styles';
 
-const FormItemListInputs = ({ formItems, setFormItems, handleBlur }) => {
-  const deleteItem = (id) => {
-    setFormItems(Object.keys(formItems).filter((item) => item !== id));
-  };
-
-  return Object.keys(formItems).map((item) => (
-    <div key={item}>
-      <Flex mb='1.8'>
-        <Field
-          as={ItemInputs}
-          handleBlur={handleBlur}
-          mr='1.5'
-          type='text'
-          name='items.name'
-          width='20'
-        />
-        <Field
-          as={ItemInputs}
-          handleBlur={handleBlur}
-          mr='1.5'
-          type='text'
-          name='items.quantity'
-          width='5'
-          min='0'
-        />
-        {/* <Field
-          as={ItemInputs}
-          handleBlur={handleBlur}
-          mr='1.5'
-          type='text'
-          name='items.price'
-          width='10'
-          defaultValue={items.price}
-        />
-        <Sum>{items.total}</Sum>
-        <IconContainer onClick={() => deleteItem(item)} ml='4'>
-          <Icon src={icon} alt='trash icon' />
-        </IconContainer> */}
-      </Flex>
-    </div>
-  ));
-};
+const FormItemListInputs = ({ values, handleBlur }) => (
+  <FieldArray
+    name='items'
+    render={(arrayHelpers) => (
+      <>
+        {values.items?.length > 0 &&
+          values.items.map((_, index) => (
+            <div key={index}>
+              <Flex mb='1.8'>
+                <Field
+                  as={ItemInputs}
+                  handleBlur={handleBlur}
+                  mr='1.5'
+                  type='text'
+                  name={`items.${index}.name`}
+                  width='20'
+                />
+                <Field
+                  as={ItemInputs}
+                  handleBlur={handleBlur}
+                  mr='1.5'
+                  type='text'
+                  name={`items.${index}.quantity`}
+                  width='5'
+                />
+                <Field
+                  as={ItemInputs}
+                  handleBlur={handleBlur}
+                  mr='1.5'
+                  type='text'
+                  name={`items.${index}.price`}
+                  width='10'
+                />
+                <Sum>0{/* TODO: calculate sum of the items */}</Sum>
+                <IconContainer
+                  onClick={() => arrayHelpers.remove(index)}
+                  ml='4'
+                >
+                  <Icon src={icon} alt='trash icon' />
+                </IconContainer>
+              </Flex>
+            </div>
+          ))}
+        <>
+          <Button
+            onClick={() =>
+              arrayHelpers.push({
+                name: '',
+                quantity: '',
+                price: ''
+              })
+            }
+            type='button'
+            sixth
+          >
+            + Add New Item
+          </Button>
+        </>
+      </>
+    )}
+  />
+);
 
 export default FormItemListInputs;
