00c3618b9b953e0e0b6f93fad42c27ee60a455ff
elshanx <elshanxakbarov@gmail.com>
2021-03-31 20:31:05 +0400

    💫 style: animate arrow on different states

--- src/components/svgs/BottomArrow.jsx
+++ src/components/svgs/BottomArrow.jsx
@@ -1,6 +1,6 @@
 import { motion } from 'framer-motion';
 
-const BottomArrow = ({ open }, props) => {
+const BottomArrow = ({ open }) => {
   const animation = {
     initial: {
       rotate: 0
@@ -10,16 +10,19 @@ const BottomArrow = ({ open }, props) => {
 
   return (
     <motion.div
-      variants={animation}
-      initial='initial'
-      animate={open ? 'inital' : 'rotate'}
+      animate={
+        open
+          ? {
+              rotate: 180
+            }
+          : { rotate: 0 }
+      }
     >
       <svg
         width='10'
         height='7'
         fill='none'
         xmlns='http://www.w3.org/2000/svg'
-        {...props}
       >
         <path
           d='M1 1l4.228 4.228L9.456 1'
