a0dbc84515a76441278ecc709368cf52a5d53f1f
elshanx <elshanxakbarov@gmail.com>
2021-04-02 19:45:28 +0400

    ➖ removed redux *

--- src/index.js
+++ src/index.js
@@ -1,17 +1,13 @@
 import App from './App';
 import { BrowserRouter } from 'react-router-dom';
-import { Provider } from 'react-redux';
 import React from 'react';
 import ReactDOM from 'react-dom';
-import store from './redux/store';
 
 ReactDOM.render(
   <BrowserRouter>
-    <Provider store={store}>
-      <React.StrictMode>
-        <App />
-      </React.StrictMode>
-    </Provider>
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>
   </BrowserRouter>,
-  document.getElementById('root'),
+  document.getElementById('root')
 );
--- src/redux/reducers/index.js
+++
@@ -1,8 +0,0 @@
-import { combineReducers } from 'redux';
-import InvoiceDetailsReducer from '../invoice-details/invoice-details.reducer';
-import invoiceReducer from '../invoices/invoices.reducer';
-
-export default combineReducers({
-  invoices: invoiceReducer,
-  invoiceDetails: InvoiceDetailsReducer,
-});
