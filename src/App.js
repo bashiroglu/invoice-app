70794bef184efb62c8f1f0d5166e172e49d6ef13
elshanx <elshanxakbarov@gmail.com>
2021-04-02 17:18:10 +0400

    ✨ feat: initialize react-query

--- src/App.js
+++ src/App.js
@@ -1,30 +1,37 @@
+import { QueryClient, QueryClientProvider } from 'react-query';
+import { ReactQueryDevtools } from 'react-query/devtools';
 import { Redirect, Route } from 'react-router-dom';
 import InvoiceDrawer from './components/invoice/invoiceDrawer/InvoiceDrawer';
 import Layout from './components/layout/Layout';
 import InvoiceDetails from './pages/invoiceDetails/InvoiceDetails';
 import Invoices from './pages/invoices/Invoices';
 
+const queryClient = new QueryClient();
+
 function App() {
   return (
-    <Layout>
-      <Route exact path='/'>
-        <Redirect to='/invoices/all' />
-      </Route>
-      <Route path='/invoices/new'>
-        <Invoices />
-        <InvoiceDrawer />
-      </Route>
-      <Route exact path='/invoices/all'>
-        <Invoices />
-      </Route>
-      <Route exact path='/invoices/all/:id'>
-        <InvoiceDetails />
-      </Route>
-      <Route path='/invoices/edit/:id/'>
-        <InvoiceDetails />
-        <InvoiceDrawer />
-      </Route>
-    </Layout>
+    <QueryClientProvider client={queryClient}>
+      <ReactQueryDevtools initialIsOpen={false} />
+      <Layout>
+        <Route exact path='/'>
+          <Redirect to='/invoices/all' />
+        </Route>
+        <Route path='/invoices/new'>
+          <Invoices />
+          <InvoiceDrawer />
+        </Route>
+        <Route exact path='/invoices/all'>
+          <Invoices />
+        </Route>
+        <Route exact path='/invoices/all/:id'>
+          <InvoiceDetails />
+        </Route>
+        <Route path='/invoices/edit/:id/'>
+          <InvoiceDetails />
+          <InvoiceDrawer />
+        </Route>
+      </Layout>
+    </QueryClientProvider>
   );
 }
 
