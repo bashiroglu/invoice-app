19bd94a04565a05250e530cc143b4f843ab932c4
elshanx <elshanxakbarov@gmail.com>
2021-04-05 19:05:45 +0400

    ✨ feat: make form dispatch submitted values

--- src/components/form/Form.jsx
+++ src/components/form/Form.jsx
@@ -1,30 +1,34 @@
 import { Formik } from 'formik';
+import { useDispatch, useSelector } from 'react-redux';
+import { useHistory } from 'react-router-dom';
 import initialValues from '../../data/initialValues';
+import { submitInvoice } from '../../redux/invoices/invoices.actions';
 import validationSchema from '../../validation/form';
 import { StyledForm } from './Form.styles';
 import FormButtonContainer from './FormButtonContainer';
 import FormFields from './FormFields';
 
 const Form = ({ id }) => {
+  const dispatch = useDispatch();
+  const state = useSelector((s) => s.invoiceDetails.invoiceDetails);
+
+  const {
+    location: { pathname }
+  } = useHistory();
+
+  const newOrEditValues = pathname.includes('edit')
+    ? state
+    : initialValues;
+
   return (
     <Formik
-      initialValues={initialValues}
+      initialValues={newOrEditValues}
       validationSchema={validationSchema}
-      // onSubmit={onFormSubmit}
       onSubmit={(values) => {
-        console.log(JSON.stringify(values, null, 2));
+        dispatch(submitInvoice(values));
       }}
     >
-      {({
-        values,
-        touched,
-        errors,
-        dirty,
-        isSubmitting,
-        handleBlur,
-        handleSubmit,
-        handleReset
-      }) => (
+      {({ values, handleBlur, handleSubmit }) => (
         <StyledForm onSubmit={handleSubmit}>
           <FormFields values={values} handleBlur={handleBlur} />
           <FormButtonContainer id={id} />
