14184196be1f38a427bcb853859391237d7026ea
elshanx <elshanxakbarov@gmail.com>
2021-04-05 15:34:27 +0400

    ♻️ refactor: separate details summary

--- src/components/invoice/invoiceDetailsSummaryTotal/InvoiceDetailsSummaryTotal.jsx
+++ src/components/invoice/invoiceDetailsSummaryTotal/InvoiceDetailsSummaryTotal.jsx
@@ -1,11 +1,16 @@
 import Skeleton from 'react-loading-skeleton';
 import { StyledInvoiceSummaryTotal } from './InvoiceDetailsSummaryTotal.style';
+import useWindowDimensions from '../../../hooks/useWindowDimensions';
 
-const InvoiceDetailsSummaryTotal = ({ invoiceDetails }) => (
-  <StyledInvoiceSummaryTotal>
-    <span>Grand Total</span>
-    <span>£ {invoiceDetails.invoiceDetails.total || <Skeleton />}</span>
-  </StyledInvoiceSummaryTotal>
-);
+const InvoiceDetailsSummaryTotal = ({ invoiceDetails }) => {
+  const { width } = useWindowDimensions();
+
+  return (
+    <StyledInvoiceSummaryTotal>
+      <span>{width < 768 ? 'Grand Total' : 'Amount Due'}</span>
+      <span>£ {invoiceDetails.invoiceDetails.total || <Skeleton />}</span>
+    </StyledInvoiceSummaryTotal>
+  );
+};
 
 export default InvoiceDetailsSummaryTotal;
